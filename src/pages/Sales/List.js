f3d05ac0b0217e2453b6920aada797ce63bb089d
bolortoli <bolortoli@csms.edu.mn>
2021-05-07 18:22:10 +0800

    Before pull

--- src/pages/Sales/List.js
+++ src/pages/Sales/List.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { MDBDataTable } from "mdbreact";
+import React, { useState, useEffect } from "react"
+import { MDBDataTable } from "mdbreact"
 import {
   Row,
   Col,
@@ -12,185 +12,185 @@ import {
   Nav,
   TabContent,
   TabPane,
-} from "reactstrap";
+} from "reactstrap"
 
-import classnames from "classnames";
+import classnames from "classnames"
 
-import "./datatables.scss";
+import "./datatables.scss"
 
-const SalesList = (props) => {
-  const [books, set_books] = useState(props.books.books);
-  const [mp3_books, set_mp3_books] = useState(props.books.book_mp3);
-  const [pdf_books, set_pdf_books] = useState(props.books.book_pdf);
+const bookColumns = [
+  {
+    label: "№",
+    field: "id",
+    sort: "disabled",
+    width: 10,
+  },
+  {
+    label: "Нэр",
+    field: "book_name",
+    width: 150,
+    sort: "asc",
+    attributes: {
+      "aria-controls": "DataTable",
+      "aria-label": "Name",
+    },
+  },
+  {
+    label: "Зохиолч",
+    field: "author",
+    sort: "asc",
+    width: 100,
+  },
+  {
+    label: "Борлуулалт",
+    field: "buy_count",
+    sort: "asc",
+    width: 70,
+  },
+  {
+    label: "Хүсэлтийн огноо",
+    field: "date",
+    sort: "disabled",
+    width: 100,
+  },
+]
 
-  const [activeTab, setactiveTab] = useState("1");
+const mp3Columns = [
+  {
+    label: "№",
+    field: "id",
+    sort: "disabled",
+    width: 10,
+  },
+  {
+    label: "Нэр",
+    field: "book_name",
+    width: 150,
+    sort: "asc",
+    attributes: {
+      "aria-controls": "DataTable",
+      "aria-label": "Name",
+    },
+  },
+  {
+    label: "Зохиолч",
+    field: "author",
+    sort: "asc",
+    width: 100,
+  },
+  {
+    label: "Борлуулалт",
+    field: "buy_count",
+    sort: "asc",
+    width: 70,
+  },
+  {
+    label: "Хүсэлтийн огноо",
+    field: "date",
+    sort: "disabled",
+    width: 100,
+  },
+]
+
+const pdfColumns = [
+  {
+    label: "№",
+    field: "id",
+    sort: "disabled",
+    width: 10,
+  },
+  {
+    label: "Нэр",
+    field: "book_name",
+    width: 150,
+    sort: "asc",
+    attributes: {
+      "aria-controls": "DataTable",
+      "aria-label": "Name",
+    },
+  },
+  {
+    label: "Зохиолч",
+    field: "author",
+    sort: "asc",
+    width: 100,
+  },
+  {
+    label: "Борлуулалт",
+    field: "buy_count",
+    sort: "asc",
+    width: 70,
+  },
+  {
+    label: "Хүсэлтийн огноо",
+    field: "date",
+    sort: "disabled",
+    width: 100,
+  },
+]
+
+const SalesList = props => {
+  const [books, set_books] = useState(props.books.books)
+  const [mp3_books, set_mp3_books] = useState(props.books.book_mp3)
+  const [pdf_books, set_pdf_books] = useState(props.books.book_pdf)
+
+  const [activeTab, setactiveTab] = useState("1")
 
   function toggle(tab) {
     if (activeTab !== tab) {
-      setactiveTab(tab);
+      setactiveTab(tab)
     }
   }
 
-  const bookColumns = [
-    {
-      label: "№",
-      field: "id",
-      sort: "disabled",
-      width: 10,
-    },
-    {
-      label: "Нэр",
-      field: "book_name",
-      width: 150,
-      sort: "asc",
-      attributes: {
-        "aria-controls": "DataTable",
-        "aria-label": "Name",
-      },
-    },
-    {
-      label: "Зохиолч",
-      field: "author",
-      sort: "asc",
-      width: 100,
-    },
-    {
-      label: "Борлуулалт",
-      field: "buy_count",
-      sort: "asc",
-      width: 70,
-    },
-    {
-      label: "Хүсэлтийн огноо",
-      field: "date",
-      sort: "disabled",
-      width: 100,
-    },
-  ];
-
-  const mp3Columns = [
-    {
-      label: "№",
-      field: "id",
-      sort: "disabled",
-      width: 10,
-    },
-    {
-      label: "Нэр",
-      field: "book_name",
-      width: 150,
-      sort: "asc",
-      attributes: {
-        "aria-controls": "DataTable",
-        "aria-label": "Name",
-      },
-    },
-    {
-      label: "Зохиолч",
-      field: "author",
-      sort: "asc",
-      width: 100,
-    },
-    {
-      label: "Борлуулалт",
-      field: "buy_count",
-      sort: "asc",
-      width: 70,
-    },
-    {
-      label: "Хүсэлтийн огноо",
-      field: "date",
-      sort: "disabled",
-      width: 100,
-    },
-  ];
-
-  const pdfColumns = [
-    {
-      label: "№",
-      field: "id",
-      sort: "disabled",
-      width: 10,
-    },
-    {
-      label: "Нэр",
-      field: "book_name",
-      width: 150,
-      sort: "asc",
-      attributes: {
-        "aria-controls": "DataTable",
-        "aria-label": "Name",
-      },
-    },
-    {
-      label: "Зохиолч",
-      field: "author",
-      sort: "asc",
-      width: 100,
-    },
-    {
-      label: "Борлуулалт",
-      field: "buy_count",
-      sort: "asc",
-      width: 70,
-    },
-    {
-      label: "Хүсэлтийн огноо",
-      field: "date",
-      sort: "disabled",
-      width: 100,
-    },
-  ];
-
-  const initBookData = (books) => {
-    let tempInitialData = books.map((d) => {
+  const initBookData = books => {
+    let tempInitialData = books.map(d => {
       return {
         id: d.id,
         book_name: d.book_name,
         author: d.book_author_name,
         buy_count: d.buy_count,
         date: new Date(d.book_added_date).toLocaleString(),
-      };
-    });
-    set_books(tempInitialData);
-  };
+      }
+    })
+    set_books(tempInitialData)
+  }
 
-  const initMp3kData = (books) => {
-    let tempInitialData = books.map((d) => {
+  const initMp3kData = books => {
+    let tempInitialData = books.map(d => {
       return {
         id: d.id,
         book_name: d.book_name,
         author: d.book_author_name,
         buy_count: d.buy_count,
         date: new Date(d.book_added_date).toLocaleString(),
-      };
-    });
-    set_mp3_books(tempInitialData);
-  };
+      }
+    })
+    set_mp3_books(tempInitialData)
+  }
 
-  const initPdfData = (books) => {
-    let tempInitialData = books.map((d) => {
+  const initPdfData = books => {
+    let tempInitialData = books.map(d => {
       return {
         id: d.id,
         book_name: d.book_name,
         author: d.book_author_name,
         buy_count: d.buy_count,
         date: new Date(d.book_added_date).toLocaleString(),
-      };
-    });
-    set_pdf_books(tempInitialData);
-  };
+      }
+    })
+    set_pdf_books(tempInitialData)
+  }
 
-  const bookDatatable = { columns: bookColumns, rows: books };
-  const mp3Datatable = { columns: bookColumns, rows: books };
-  const pdfDatatable = { columns: bookColumns, rows: books };
+  const bookDatatable = { columns: bookColumns, rows: books }
+  const mp3Datatable = { columns: bookColumns, rows: books }
+  const pdfDatatable = { columns: bookColumns, rows: books }
 
   useEffect(() => {
-    initBookData(books);
-    initMp3kData(mp3_books);
-    initPdfData(pdf_books);
+    initBookData(books)
+    initMp3kData(mp3_books)
+    initPdfData(pdf_books)
     // fetchData();
-  }, []);
+  }, [])
 
   return (
     <React.Fragment>
@@ -210,7 +210,7 @@ const SalesList = (props) => {
                         active: activeTab === "1",
                       })}
                       onClick={() => {
-                        toggle("1");
+                        toggle("1")
                       }}
                     >
                       Ном
@@ -223,7 +223,7 @@ const SalesList = (props) => {
                         active: activeTab === "2",
                       })}
                       onClick={() => {
-                        toggle("2");
+                        toggle("2")
                       }}
                     >
                       Аудио ном
@@ -236,7 +236,7 @@ const SalesList = (props) => {
                         active: activeTab === "3",
                       })}
                       onClick={() => {
-                        toggle("3");
+                        toggle("3")
                       }}
                     >
                       Цахим ном
@@ -284,7 +284,7 @@ const SalesList = (props) => {
         </Row>
       </div>
     </React.Fragment>
-  );
-};
+  )
+}
 
-export default SalesList;
+export default SalesList
